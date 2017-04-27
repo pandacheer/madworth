a5ba652c5b12aec5fbaff07d99aae7a45deecd0b
pandacheer <305046350@qq.com>
2017-04-27 09:16:20 +0800

    Delete email.php

--- pdqadmin/application/config/email.php
+++
@@ -1,9 +0,0 @@
-<?php
-$config['protocol'] = 'smtp';
-$config['smtp_host'] = 'smtp.sendgrid.net';
-$config['smtp_user'] = 'dgcart';
-$config['smtp_pass'] = '1dayinthelife';
-$config['smtp_port'] = 587;
-$config['crlf'] = '\r\n';
-$config['newline'] = '\r\n';
-$config['mailtype'] = 'html';
